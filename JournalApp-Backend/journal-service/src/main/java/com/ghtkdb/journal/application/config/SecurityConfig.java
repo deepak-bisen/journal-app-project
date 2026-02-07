b7c22e39fdde10b7b6450ff43b79b2810fb42ef4
Deepak14-12 <bisen1412@gmail.com>
2026-02-08 01:05:44 +0530

    updated

--- JournalApp-Backend/journal-service/src/main/java/com/ghtkdb/journal/application/config/SecurityConfig.java
+++ JournalApp-Backend/journal-service/src/main/java/com/ghtkdb/journal/application/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
         // Allow your specific frontend origin
         configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://localhost:5500","http://localhost:3000", "https://your-deployed-frontend.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders("*");
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
